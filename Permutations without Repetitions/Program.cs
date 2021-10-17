a20c368db6bc6d83617bc5fc0d9ede4de543463e
danail2003 <ddobrev20@outlook.com>
2021-10-17 20:05:40 +0300

    added permutation problem

---
+++ Permutations without Repetitions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Permutations_without_Repetitions
+{
+    class Program
+    {
+        private static string[] permutations;
+        private static bool[] used;
+        private static string[] elements;
+
+        static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+
+            Permut(0);
+        }
+
+        private static void Permut(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(' ', permutations));
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        permutations[index] = elements[i];
+                        Permut(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
