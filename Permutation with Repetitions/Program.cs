bbf53efb15f1d0d1f4c455de680efe8bcb628d68
danail2003 <ddobrev20@outlook.com>
2021-10-19 20:23:27 +0300

    added variations problems and permutation with repetitions

---
+++ Permutation with Repetitions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutation_with_Repetitions
+{
+    class Program
+    {
+        private static string[] elements;
+        private static HashSet<string> permutations;
+
+        static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            permutations = new HashSet<string>();
+
+            Permute(0);
+
+            foreach (var permutation in permutations)
+            {
+                Console.WriteLine(string.Join(' ', permutation));
+            }
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                permutations.Add(string.Join(' ', elements));
+                return;
+            }
+
+            Permute(index + 1);
+
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            string temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
---
+++ Variations with Repetitions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Variations_with_Repetitions
+{
+    class Program
+    {
+        private static string[] elements;
+        private static string[] variations;
+
+        static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            int k = int.Parse(Console.ReadLine());
+            variations = new string[k];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(' ', variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
