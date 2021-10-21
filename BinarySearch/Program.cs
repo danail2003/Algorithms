566d8e3fd6ff44cde3f8876e2ae37102d33aa08a
danail2003 <ddobrev20@outlook.com>
2021-10-21 20:01:58 +0300

    added binary search and selection sort problem

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int searchedNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(numbers, searchedNumber));
+        }
+
+        private static int BinarySearch(int[] numbers, int searchedNumber)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                int element = numbers[mid];
+
+                if (element == searchedNumber)
+                {
+                    return mid;
+                }
+
+                if (searchedNumber > element)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SelectionSort(array);
+
+            Console.WriteLine(string.Join(' ', array));
+        }
+
+        private static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int index = i;
+                int minIndex = 0;
+                int minValue = int.MaxValue;
+
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] < minValue)
+                    {
+                        minValue = array[j];
+                        minIndex = j;
+                    }
+                }
+
+                Swap(index, minIndex, array);
+            }
+        }
+
+        private static void Swap(int index, int minIndex, int[] array)
+        {
+            int temp = array[index];
+            array[index] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
