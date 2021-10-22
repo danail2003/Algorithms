d54ca9dd7328a5c316cafc0457c740ddbec1d37b
danail2003 <ddobrev20@outlook.com>
2021-10-22 18:42:59 +0300

    added bubble sort and insertion sort problems

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            bool isSorted = false;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        Swap(i, i + 1, numbers);
+                        isSorted = false;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int firstIndex, int secondIndex, int[] numbers)
+        {
+            int temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            InsertionSort(numbers);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static void InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+                int index = i;
+
+                for (int j = i; j > 0; j--)
+                {
+                    if (number < numbers[j - 1])
+                    {
+                        Swap(numbers, index, j - 1);
+                        index--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] numbers, int firstIndex, int secondIndex)
+        {
+            int temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+        }
+    }
+}
