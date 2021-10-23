ad03f61b5562dc3f9e756d426d420abc5e948eef
danail2003 <ddobrev20@outlook.com>
2021-10-23 11:27:12 +0300

    quick sort is implemented

---
+++ QuickSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            QuickSort(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static void QuickSort(int[] numbers, int startIdx, int endIdx)
+        {
+            if (startIdx >= endIdx)
+            {
+                return;
+            }
+
+            int pivot = startIdx;
+            int left = startIdx + 1;
+            int right = endIdx;
+
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot] && numbers[right] < numbers[pivot])
+                {
+                    Swap(numbers, left, right);
+                }
+
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left += 1;
+                }
+
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right -= 1;
+                }
+            }
+
+            Swap(numbers, pivot, right);
+
+            bool isLeftSubArraySmaller = right - 1 - startIdx < endIdx - (right + 1);
+
+            if (isLeftSubArraySmaller)
+            {
+                QuickSort(numbers, startIdx, right - 1);
+                QuickSort(numbers, right + 1, endIdx);
+            }
+            else
+            {
+                QuickSort(numbers, right + 1, endIdx);
+                QuickSort(numbers, startIdx, right - 1);
+            }
+        }
+
+        private static void Swap(int[] numbers, int left, int right)
+        {
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+        }
+    }
+}
