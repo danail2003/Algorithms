4baf94dc5a7c75cea3dfbc5aa659d973570e8740
danail2003 <ddobrev20@outlook.com>
2021-10-23 18:35:27 +0300

    implemented merge sort and sum of coins which is greedy algorithm

---
+++ MergeSort/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sortedNumbers = MergeSort(numbers);
+
+            Console.WriteLine(string.Join(' ', sortedNumbers));
+        }
+
+        private static int[] MergeSort(int[] numbers)
+        {
+            if (numbers.Length == 1)
+            {
+                return numbers;
+            }
+
+            int[] left = numbers.Take(numbers.Length / 2).ToArray();
+            int[] right = numbers.Skip(numbers.Length / 2).ToArray();
+
+            return MergeArrays(MergeSort(left), MergeSort(right));
+        }
+
+        private static int[] MergeArrays(int[] left, int[] right)
+        {
+            int[] mergedArray = new int[left.Length + right.Length];
+            int mergedIdx = 0;
+            int leftIdx = 0;
+            int rightIdx = 0;
+
+            while (leftIdx < left.Length && rightIdx < right.Length)
+            {
+                if (left[leftIdx] < right[rightIdx])
+                {
+                    mergedArray[mergedIdx++] = left[leftIdx++];
+                }
+                else
+                {
+                    mergedArray[mergedIdx++] = right[rightIdx++];
+                }
+            }
+
+            while (leftIdx < left.Length)
+            {
+                mergedArray[mergedIdx++] = left[leftIdx++];
+            }
+
+            while (rightIdx < right.Length)
+            {
+                mergedArray[mergedIdx++] = right[rightIdx++];
+            }
+
+            return mergedArray;
+        }
+    }
+}
---
+++ SumOfCoins/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SumOfCoins
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] coins = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new ();
+
+            int counter = 0;
+            int index = 0;
+            coins = coins.OrderByDescending(x => x).ToArray();
+
+            while (target > 0 && index < coins.Length)
+            {
+                int currentCoin = coins[index++];
+                int coinsCount = target / currentCoin;
+
+                if (coinsCount > 0)
+                {
+                    counter += coinsCount;
+                    target -= currentCoin * coinsCount;
+                    sb.AppendLine($"{coinsCount} coin(s) with value {currentCoin}");
+                }
+            }
+
+            if (target == 0)
+            {
+                Console.WriteLine($"Number of coins to take: {counter}");
+                Console.WriteLine(sb.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
