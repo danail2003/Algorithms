e52ac2e264809770bef844a91d04e342098c8f95
danail2003 <ddobrev20@outlook.com>
2021-11-13 19:33:47 +0200

    dynamic programming problems implemented

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private static Dictionary<int, long> cache;
+
+        static void Main()
+        {
+            cache = new Dictionary<int, long>();
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(number));
+        }
+
+        private static long Fibonacci(int number)
+        {
+            if (cache.ContainsKey(number))
+            {
+                return cache[number];
+            }
+
+            if (number <= 2)
+            {
+                return 1;
+            }
+
+            long result = Fibonacci(number - 1) + Fibonacci(number - 2);
+
+            cache[number] = result;
+
+            return result;
+        }
+    }
+}
---
+++ LongestCommonSubsequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LongestCommonSubsequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+
+            int[,] matrix = new int[firstString.Length + 1, secondString.Length + 1];
+
+            for (int r = 1; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 1; c < matrix.GetLength(1); c++)
+                {
+                    if (firstString[r - 1] == secondString[c - 1])
+                    {
+                        matrix[r, c] = matrix[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[r, c] = Math.Max(matrix[r - 1, c], matrix[r, c - 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(matrix[firstString.Length, secondString.Length]);
+        }
+    }
+}
