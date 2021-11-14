db0b2a7239468e12de897e2156e94ca8803402cd
danail2003 <ddobrev20@outlook.com>
2021-11-14 16:16:05 +0200

    added move down right sum problem

---
+++ MoveDownRightSum/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoveDownRightSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            int[,] sums = new int[rows, cols];
+            sums[0, 0] = matrix[0, 0];
+
+            for (int c = 1; c < cols; c++)
+            {
+                sums[0, c] = sums[0, cols - 1] + matrix[0, c];
+            }
+
+            for (int r = 1; r < cols; r++)
+            {
+                sums[r, 0] = sums[r - 1, 0] + matrix[r, 0];
+            }
+
+            for (int r = 1; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 1; c < matrix.GetLength(1); c++)
+                {
+                    int upperCell = sums[r - 1, c];
+                    int leftCell = sums[r, c - 1];
+
+                    sums[r, c] = Math.Max(upperCell, leftCell) + matrix[r, c];
+                }
+            }
+
+            int row = rows - 1;
+            int col = cols - 1;
+            Stack<string> path = new();
+            path.Push($"[{row}, {col}]");
+
+            while (row > 0 && col > 0)
+            {
+                int upper = sums[row - 1, col];
+                int left = sums[row, col - 1];
+
+                if (upper > left)
+                {
+                    row -= 1;
+                }
+                else
+                {
+                    col -= 1;
+                }
+
+                path.Push($"[{row}, {col}]");
+            }
+
+            while (row > 0)
+            {
+                row -= 1;
+                path.Push($"[{row}, {col}]");
+            }
+
+            while (col > 0)
+            {
+                col -= 1;
+                path.Push($"[{row}, {col}]");
+            }
+
+            Console.WriteLine(string.Join(' ', path));
+        }
+    }
+}
---
+++ SubsetSumWithRepetitions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SubsetSumWithRepetitions
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+
+            bool[] sums = new bool[target + 1];
+            sums[0] = true;
+
+            for (int sum = 0; sum < sums.Length; sum++)
+            {
+                if (sums[sum])
+                {
+                    foreach (var number in numbers)
+                    {
+                        int newSum = number + sum;
+                        if (newSum <= target)
+                        {
+                            sums[newSum] = true;
+                        }
+                    }
+                }
+            }
+
+            while (target > 0)
+            {
+                foreach (var number in numbers)
+                {
+                    int previousSum = target - number;
+                    if (previousSum >= 0 && sums[previousSum])
+                    {
+                        Console.WriteLine(number);
+                        target = previousSum;
+                    }
+                }
+            }
+        }
+    }
+}
